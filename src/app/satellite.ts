6c14cdd00aa941068dc7fbaa56bf39fffa056a91
Matt R <mattreimer1@yahoo.com>
2020-12-14 03:50:10 -0600

    formatting changes

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,5 +1,3 @@
-// import{ } from './app.component'
-
 export class Satellite {
     name: string;
     orbitType: string;
@@ -7,15 +5,12 @@ export class Satellite {
     operational: boolean;
     launchDate: string;
     
-
-
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
         this.name= name;
         this.type= type;
         this.launchDate= launchDate;
         this.orbitType= orbitType;
-        this.operational= operational;
-        
+        this.operational= operational;    
     }
 
     shouldShowWarning(): boolean  {
